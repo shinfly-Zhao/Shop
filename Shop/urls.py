12a8d0ec82cb2545894bc0d9ae4e43e606222174
shinfly-Zhao <1697178173@qq.com>
2019-09-05 17:06:22 +0800

    添加 - 手机注册 类别API 商品API 收藏API

--- Shop/urls.py
+++ Shop/urls.py
@@ -15,7 +15,32 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from rest_framework.documentation import include_docs_urls  # 文档
+import xadmin
+from django.views.static import serve
+from Shop.settings import MEDIA_ROOT,STATIC_ROOT
+from rest_framework.routers import DefaultRouter
+from goods.views import *
+from users.views import *
+from user_operation.views import *
+from rest_framework_jwt.views import obtain_jwt_token
+router = DefaultRouter()
+router.register("categorys",GoodsCategoryViewSet,base_name="categorys")  # 配置商品分类的url
+router.register("goods",GoodsViewSet,base_name="goods")  # 商品url
+router.register("code",SmsCodeViewSet,base_name="code")  # 短信验证码
+router.register("reg",UserRegistViewSet,base_name="reg")  # 短信验证码
+router.register("userfavs",UserFavViewset,base_name="userfavs")  # 配置用户收藏
 
+from rest_framework_swagger.views import get_swagger_view
+schema_view = get_swagger_view(title="我的docs")
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    url(r'^shopdocs/', include_docs_urls(title='购物商品API')),
+    url(r'^swagger/', schema_view),
+    path('xadmin/', xadmin.site.urls),
+    url(r'^media/(?P<path>.*)', serve, {"document_root": MEDIA_ROOT}),  # 媒体文件
+    url(r'^static/(?P<path>.*)', serve, {"document_root": STATIC_ROOT}),  # 静态文件
+    url(r'^', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+    path('login/',obtain_jwt_token),
 ]
